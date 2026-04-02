801d11dbfaa5bee7458627361641b08adf5b2cd5
Lakshya Goyal <lakshyagoyal34@gmail.com>
2026-04-03 01:44:56 +0530

    Type Error Cleared -1

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,25 @@
-// next.config.ts
-import type { NextConfig } from 'next';
-import { Configuration } from 'webpack';
+import type { NextConfig } from "next";
+import { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  // Modern Turbopack config (correct syntax)
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.jsx'
-        }
-      }
-    }
-  },
-
-  // Webpack fallback config
   webpack: (config: Configuration) => {
     config.module?.rules?.push({
       test: /\.svg$/i,
-      use: ['@svgr/webpack']
+      use: ["@svgr/webpack"],
     });
     return config;
   },
 
-  // Disable static image optimization for SVGs
   images: {
     disableStaticImages: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "arthur-01-09-2025.s3.ap-south-1.amazonaws.com",
-        pathname: "/**"
-      }
-    ]
-  }
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
